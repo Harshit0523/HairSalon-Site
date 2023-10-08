eb3b2145948a3950c9cf44f40fbbfde43d0d7cbf
Harshit0523 <harshupahade2004@gmail.com>
2023-10-08 09:47:44 +0530

    add some styles

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Gallery from './components/Gallery';
 import Footer from './components/Footer';
 import Our2 from './components/Our2';
 import Contact from './components/Contact';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { BiLogoFacebook } from 'react-icons/bi';
+import { ImWhatsapp } from 'react-icons/im';
 
 
 function App() {
@@ -36,14 +39,26 @@ function App() {
   });
   return (
     <>
-     <section className='h-screen relative bg-gradient-to-t from-slate-900 ' id='Home'>
-    <img src={img} className='h-full w-full object-cover absolute mix-blend-overlay ' />
+     <section className='h-screen relative bg-gradient-to-r from-black to-slate-800  ' id='Home'>
+    <img src={img} className='h-full w-full object-cover absolute mix-blend-multiply ' />
     <Navbar textcolor={textcolor} scroll={scroll}/>
-    <h1 className='md:text-9xl text-8xl text-center bold mt-32 font-mono text-white  '>CLASS-1</h1>
+    <h1 className='md:text-9xl text-8xl text-center bold mt-32 font-sans  text-white '><i><b className='text-red-500'>CLASS-1</b></i></h1>
     <h1 className='text-5xl text-center bold mt-4 font-mono text-white'>UNISEX SALON</h1>
-    <h1 className='text-xl text-center bold mt-4 font-mono text-white'>UNISEX SALON</h1>
-    </section>
 
+
+
+    <div className='text-center  justify-center text-white bold flex py-6 text-2xl'>
+
+        <div className='m-1'> <AiOutlineInstagram/>  </div>
+        <div  className='m-1'> <BiLogoFacebook/> </div>
+        <div  className='m-1'> <ImWhatsapp/></div>
+        
+           
+
+
+  
+ </div>
+    </section>
     <section id='About'>
 
     <About/>
